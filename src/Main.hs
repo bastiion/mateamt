07bd9abde34758e0ff897898dfc2a21f4a5f7a49
nek0 <nek0@nek0.eu>
2019-08-10 10:37:45 +0200

    commence cotrolify

--- src/Main.hs
+++ src/Main.hs
@@ -39,6 +39,7 @@ import API
 import Model as M
 
 import Types
+import Control
 
 main :: IO ()
 main = do
@@ -229,64 +230,3 @@ productList :: MateHandler [ProductOverview]
 productList = do
   conn <- rsConnection <$> ask
   productOverviewSelect conn
-
-
-buy (Just auid) pds = do
-  conn <- rsConnection <$> ask
-  (missing, real) <- foldM (\acc@(ms, rs) pd -> do
-    mmiss <- checkProductAvailability pd conn
-    case mmiss of
-      Just miss -> return
-        ( (pd {purchaseDetailAmount = miss}):ms
-        , (pd {purchaseDetailAmount = max 0 (purchaseDetailAmount pd - miss)}:rs)
-        )
-      Nothing -> return
-        ( ms
-        , pd:rs
-        )
-    )
-    ([], [])
-    pds
-  void $ mapM_ (\pd -> postBuyProductAmountUpdate pd conn) real
-  price <- foldM
-    (\total pd ->
-      fmap (+ total) (getLatestTotalPrice pd conn)
-    )
-    0
-    real
-  addToUserBalance auid (-price) conn
-  newBalance <- userBalanceSelect conn auid
-  return $ PurchaseResult
-    ( if newBalance < 0
-      then PurchaseDebtful
-      else PurchaseOK
-    )
-    missing
-buy Nothing pds = do
-  conn <- rsConnection <$> ask
-  (missing, real) <- foldM (\acc@(ms, rs) pd -> do
-    mmiss <- checkProductAvailability pd conn
-    case mmiss of
-      Just miss -> return
-        ( (pd {purchaseDetailAmount = miss}):ms
-        , (pd {purchaseDetailAmount = max 0 (purchaseDetailAmount pd - miss)}:rs)
-        )
-      Nothing -> return
-        ( ms
-        , pd:rs
-        )
-    )
-    ([], [])
-    pds
-  void $ mapM_
-    (\pd -> postBuyProductAmountUpdate pd conn)
-    real
-  price <- foldM
-    (\total pd ->
-      fmap (+ total) (getLatestTotalPrice pd conn)
-    )
-    0
-    real
-  return $ PurchaseResult
-    (PayAmount price)
-    missing
