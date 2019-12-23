2f3ef14a97da55e319f64f880396a6621a29e6e2
nek0 <nek0@nek0.eu>
2019-12-23 06:34:46 +0100

    add lib and config

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 { pkgs ? import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz))
 , haskellCompiler ? "ghc865"
+, lib ? pkgs.lib
+, config ? pkgs.config
 }:
   pkgs.haskell-nix.cabalProject {
     src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
