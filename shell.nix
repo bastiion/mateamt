583316224c1c018a37f78b3825800431f19f503c
nek0 <nek0@nek0.eu>
2020-05-10 07:28:17 +0200

    start slowly

--- shell.nix
+++ shell.nix
@@ -5,12 +5,13 @@ let
   inherit (nixpkgs) pkgs;
 
   f = { mkDerivation, aeson, base, base16-bytestring
-      , base64-bytestring, bytestring, case-insensitive, containers
-      , HsYAML, http-api-data, http-types, iproute, mtl, network, opaleye
-      , optparse-applicative, postgresql-simple, product-profunctors
-      , profunctors, pureMD5, random-bytestring, servant, servant-rawm
-      , servant-server, stdenv, stm, text, time, wai, wai-logger
-      , wai-middleware-throttle, warp
+      , base64-bytestring, bytestring, case-insensitive, clock
+      , containers, HsYAML, http-api-data, http-types, iproute, mtl
+      , network, opaleye, optparse-applicative, postgresql-simple
+      , postgresql-simple-migration, product-profunctors, profunctors
+      , pureMD5, random-bytestring, servant, servant-rawm, servant-server
+      , stdenv, stm, text, time, wai, wai-logger, wai-middleware-throttle
+      , warp
       }:
       mkDerivation {
         pname = "mateamt";
@@ -25,10 +26,11 @@ let
           servant-rawm servant-server stm text time wai wai-logger warp
         ];
         executableHaskellDepends = [
-          base base16-bytestring bytestring case-insensitive containers
+          base base16-bytestring bytestring case-insensitive clock containers
           HsYAML iproute mtl network opaleye optparse-applicative
-          postgresql-simple servant servant-server stm text time wai
-          wai-logger wai-middleware-throttle warp
+          postgresql-simple postgresql-simple-migration servant
+          servant-server stm text time wai wai-logger wai-middleware-throttle
+          warp
         ];
         description = "A whole new matemat";
         license = stdenv.lib.licenses.agpl3;
